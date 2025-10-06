ea225ac3a4b0257d34ef8c9196ac67f5a84f5fd0
Nandros <nathan.andre.etudiant@gmail.com>
2025-10-06 11:34:41 +0200

    Reading and Writing Exemple

--- main.cpp
+++ main.cpp
@@ -14,19 +14,24 @@ int main(int argc, char const *argv[])
 
     std::cout << "Project Path :" << PROJECT_PATH << "\n";
 
-    std::cout << "Get assets" << "\n";
+    std::cout << "===== Reading Files" << "\n";
     std::cout << std::string(PROJECT_PATH) + std::string(LES_MISERABLES_PATH) << "\n";
     std::cout << std::string(PROJECT_PATH) + std::string(NOTRE_DAME_DE_PARIS_PATH) << "\n";
     
-    util::readFileIntoString(std::string(PROJECT_PATH) + std::string(LES_MISERABLES_PATH), lesMiserables);
+    ret = util::readFileIntoString(std::string(PROJECT_PATH) + std::string(LES_MISERABLES_PATH), lesMiserables);
     std::cout << "Les Miserables : " << (ret ? "Success" : "Error") << "\n";
-    
-    util::readFileIntoString(std::string(PROJECT_PATH) + std::string(NOTRE_DAME_DE_PARIS_PATH), notreDameDeParis);
-    std::cout << "Les Miserables length : " << lesMiserables.length() << "\n";
 
+    ret = util::readFileIntoString(std::string(PROJECT_PATH) + std::string(NOTRE_DAME_DE_PARIS_PATH), notreDameDeParis);
+    std::cout << "Les Miserables length : " << lesMiserables.length() << "\n";
 
     std::cout << "Notre Dame De Paris : " << (ret ? "Success" : "Error") << "\n";
     std::cout << "Notre Dame De Paris length : " << notreDameDeParis.length() << "\n";
+
+    std::cout << "===== Writing Files" << "\n";
+    std::string testWrite = "JACKOOODFSFDSJAKFHSDKJGFBULDGHBSR\nwagyula\n[esdfds,fdss fdsdfs, 125]";
+    std::cout << "Write into " << std::string(PROJECT_PATH) + TEST_LEXIQUE_PATH << "\n";
+    ret = util::writeStringIntoFile(std::string(PROJECT_PATH) + TEST_LEXIQUE_PATH, testWrite);
+    std::cout << "Write Success : " << (ret ? "Success" : "Error") << "\n";
 
     return 0;
 }
