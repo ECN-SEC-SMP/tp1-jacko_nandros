f9288acd7a28719be9efed3e3ec8d2f763874b40
Jaaackoo <jack.hascoet@gmail.com>
2025-10-12 16:47:43 +0200

    test de Lexique

--- main.cpp
+++ main.cpp
@@ -11,34 +11,50 @@ std::string lesMiserables;
 std::string notreDameDeParis;
 
 void test_ReadingWriting(void);
-
+void test_addWordLexique(void);
+void test_removeWordLexique(void);
+void test_nbOfOccurencyLexique(void);
+void test_lengthLexique(void);
+void test_getContentLexique(void);
+void test_operatorOutLexique(void);
+void test_operatorAddLexique(void);
+void test_operatorSubLexique(void);
 int main(int argc, char const *argv[])
 {
-    bool ret;
-    
-    // Tests
-    test_ReadingWriting();
 
-    // Reading lesMiseables
-    std::cout << "===== Get lesMiserables" << "\n";
-    ret = util::readFileIntoString(std::string(PROJECT_PATH) + std::string(LES_MISERABLES_PATH), lesMiserables);
-    assert(ret);
-    
-    std::cout << "===== Create Lexique Object" << "\n";
-    Lexique lexique;
-    std::cout << "===== Lexique Read book" << "\n";
-    lexique.readBook(lesMiserables);
+    // test_addWordLexique();
+    // test_removeWordLexique();
+    // test_nbOfOccurencyLexique();
+    // test_lengthLexique();
+    // test_getContentLexique();
+    // test_operatorOutLexique();
+    // test_operatorAddLexique();
+    test_operatorSubLexique();
+    // bool ret;
 
-    std::cout << "===== Print lexique" << "\n";
-    std::cout << "Length of Lexique : " << lexique.length() << "\n";
+    // // Tests
+    // test_ReadingWriting();
 
-    // std::cout << lexique.getContent() << "\n";
-    util::writeStringIntoFile(std::string(PROJECT_PATH) + std::string(LES_MISERABLES_LEXIQUE_PATH), lexique.getContent());
+    // // Reading lesMiseables
+    // std::cout << "===== Get lesMiserables" << "\n";
+    // ret = util::readFileIntoString(std::string(PROJECT_PATH) + std::string(LES_MISERABLES_PATH), lesMiserables);
+    // assert(ret);
+
+    // std::cout << "===== Create Lexique Object" << "\n";
+    // Lexique lexique;
+    // std::cout << "===== Lexique Read book" << "\n";
+    // lexique.readBook(lesMiserables);
+    // std::cout << "===== Print lexique" << "\n";
+    // std::cout << "Length of Lexique : " << lexique.length() << "\n";
+
+    // // std::cout << lexique.getContent() << "\n";
+    // util::writeStringIntoFile(std::string(PROJECT_PATH) + std::string(LES_MISERABLES_LEXIQUE_PATH), lexique.getContent());
 
     return 0;
 }
 
-void test_ReadingWriting(void) {
+void test_ReadingWriting(void)
+{
     bool ret;
 
     std::cout << "Project Path :" << PROJECT_PATH << "\n";
@@ -46,7 +62,7 @@ void test_ReadingWriting(void) {
     std::cout << "===== Reading Files" << "\n";
     std::cout << std::string(PROJECT_PATH) + std::string(LES_MISERABLES_PATH) << "\n";
     std::cout << std::string(PROJECT_PATH) + std::string(NOTRE_DAME_DE_PARIS_PATH) << "\n";
-    
+
     ret = util::readFileIntoString(std::string(PROJECT_PATH) + std::string(LES_MISERABLES_PATH), lesMiserables);
     std::cout << "Les Miserables : " << (ret ? "Success" : "Error") << "\n";
 
@@ -64,3 +80,103 @@ void test_ReadingWriting(void) {
 
     return;
 }
+
+void test_addWordLexique(void)
+{
+
+    Lexique l1 = Lexique();
+    l1.addWord("Mot_1");
+    l1.addWord("Mot_1");
+    l1.addWord("Mot_2");
+    std::map<std::string, int> map = l1.getLexique();
+    assert(map["Mot_1"] == 2);
+    assert(map["Mot_2"] == 1);
+}
+
+void test_removeWordLexique(void)
+{
+
+    Lexique l1 = Lexique();
+    l1.addWord("Mot_1");
+    l1.addWord("Mot_1");
+    l1.addWord("Mot_2");
+
+    l1.remove("Mot_1"); // First removal
+    l1.remove("Mot_1"); // Try  to remove a non-existing word
+
+    std::map<std::string, int> map = l1.getLexique();
+    assert(map["Mot_2"] == 1);
+}
+
+void test_nbOfOccurencyLexique(void)
+{
+    Lexique l1 = Lexique();
+    l1.addWord("Mot_1");
+    l1.addWord("Mot_1");
+    l1.addWord("Mot_2");
+
+    assert(l1.nbOfOccurency("Mot_1") == 2);
+    assert(l1.nbOfOccurency("Mot_2") == 1);
+    assert(l1.nbOfOccurency("Mot_3") == -1);
+}
+
+void test_lengthLexique(void)
+{
+    Lexique l1 = Lexique();
+    Lexique l2 = Lexique();
+    l1.addWord("Mot_1");
+    l1.addWord("Mot_1");
+    l1.addWord("Mot_2");
+
+    assert(l1.length() == 2);
+    assert(l2.length() == 0);
+}
+
+void test_getContentLexique(void)
+{
+    Lexique l1 = Lexique();
+    Lexique l2 = Lexique();
+    l1.addWord("Mot_1");
+    assert(l1.getContent() == "Mot_1: 1\n");
+    assert(l2.getContent() == "");
+}
+
+void test_operatorOutLexique(void)
+{
+    Lexique l1 = Lexique();
+    Lexique l2 = Lexique();
+    l1.addWord("Mot_1");
+    l1.addWord("Mot_1");
+    l1.addWord("Mot_2");
+
+    std::cout << l1;
+    std::cout << l2;
+}
+
+void test_operatorAddLexique(void)
+{
+    Lexique l1 = Lexique();
+    Lexique l2 = Lexique();
+    l1.addWord("Mot_1");
+
+    l2.addWord("Mot_1");
+    l2.addWord("Mot_2");
+
+    l1 += l2;
+    assert(l1.getContent() == "Mot_1: 2\nMot_2: 1\n");
+}
+
+void test_operatorSubLexique(void)
+{
+    Lexique l1 = Lexique();
+    Lexique l2 = Lexique();
+    l1.addWord("Mot_1");
+    l1.addWord("Mot_1");
+    l1.addWord("Mot_2");
+
+    l2.addWord("Mot_2");
+
+    l1 -= l2;
+    assert(l1.getContent() == "Mot_1: 2\n");
+}
+    
